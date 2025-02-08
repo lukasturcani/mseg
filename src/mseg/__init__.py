38f56aa90e83d670cf11c7cac0087b2ff23cfeff
Lukas Turcani <lukasturcani93@gmail.com>
2025-02-08 20:41:07 +0000

    plot

--- src/mseg/__init__.py
+++ src/mseg/__init__.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 
 import polars as pl
+import plotly.express as px
 
 
 def read_data(path: Path) -> pl.DataFrame:
@@ -16,6 +17,18 @@ def read_data(path: Path) -> pl.DataFrame:
         xs.append(float(x))
         ys.append(float(y))
     return pl.DataFrame({"x": xs, "y": ys})
+
+
+def scatter_plot(data: pl.DataFrame) -> None:
+    """Plot data."""
+    fig = px.scatter(data, x="x", y="y")
+    fig.show()
+
+
+def line_plot(data: pl.DataFrame) -> None:
+    """Plot data."""
+    fig = px.line(data, x="x", y="y")
+    fig.show()
 
 
 __all__ = ["read_data"]
