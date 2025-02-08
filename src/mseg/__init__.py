26be6e982953336b6a343e7de89103cccea08bfd
Lukas Turcani <lukasturcani93@gmail.com>
2025-02-08 21:05:00 +0000

    Added pyearth

--- src/mseg/__init__.py
+++ src/mseg/__init__.py
@@ -3,6 +3,9 @@
 from pathlib import Path
 
 import plotly.express as px
+import numpy as np
+from pyearth import Earth
+from matplotlib import pyplot
 import polars as pl
 
 
@@ -29,6 +32,33 @@ def line_plot(data: pl.DataFrame) -> None:
     """Plot data."""
     fig = px.line(data, x="time", y="power")
     fig.show()
+
+
+def pyearth() -> None:
+    # Create some fake data
+    np.random.seed(0)
+    m = 1000
+    n = 10
+    X = 80 * np.random.uniform(size=(m, n)) - 40
+    y = np.abs(X[:, 6] - 4.0) + 1 * np.random.normal(size=m)
+
+    # Fit an Earth model
+    model = Earth()
+    model.fit(X, y)
+
+    # Print the model
+    print(model.trace())
+    print(model.summary())
+
+    # Plot the model
+    y_hat = model.predict(X)
+    pyplot.figure()
+    pyplot.plot(X[:, 6], y, "r.")
+    pyplot.plot(X[:, 6], y_hat, "b.")
+    pyplot.xlabel("x_6")
+    pyplot.ylabel("y")
+    pyplot.title("Simple Earth Example")
+    pyplot.show()
 
 
 __all__ = ["read_data"]
