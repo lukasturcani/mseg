3b6afabecd23bd3f49631f80debd010b61ec3ded
Lukas Turcani <lukasturcani93@gmail.com>
2025-02-08 20:27:09 +0000

    Add data loading

--- src/mseg/__init__.py
+++ src/mseg/__init__.py
@@ -1,7 +1,33 @@
-"""A template project."""
+"""Segmentation analysis."""
 
-from mseg._internal import foo
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(slots=True)
+class Data:
+    """A dataset."""
+
+    xs: list[float]
+    """The x coordinates of the data points."""
+    ys: list[float]
+    """The y coordinates of the data points."""
+
+
+def load_data(path: Path) -> Data:
+    """Load data from a file."""
+    content = path.read_text()
+    lines = content.splitlines()
+    xs = []
+    ys = []
+    for line in lines:
+        x, y = line.split()
+        xs.append(float(x))
+        ys.append(float(y))
+    return Data(xs, ys)
+
 
 __all__ = [
-    "foo",
+    "Data",
+    "load_data",
 ]
