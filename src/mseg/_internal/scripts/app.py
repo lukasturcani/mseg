45b188c14d908fcdfd5d63019fba96f11089f157
lukas <lukas@edra.ai>
2025-05-27 21:26:47 +0100

    WIP

--- src/mseg/_internal/scripts/app.py
+++ src/mseg/_internal/scripts/app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+from mseg._internal.scripts import rbeast, ruptures
+
 
 def main() -> None:
-    st.navigation()
+    pg = st.navigation([st.Page(rbeast.__file__), st.Page(ruptures.__file__)])
+    pg.run()
 
 
 if __name__ == "__main__":
