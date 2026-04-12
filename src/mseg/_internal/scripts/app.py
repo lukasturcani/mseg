cc82689e3d579196c3513f337f44638f24234bff
Lukas Turcani <lukasturcani93@gmail.com>
2026-04-12 23:35:57 +0100

    Add permutation anova (#9)

--- src/mseg/_internal/scripts/app.py
+++ src/mseg/_internal/scripts/app.py
@@ -1,11 +1,19 @@
 import streamlit as st
 
-from mseg._internal.scripts import rbeast_app, ruptures_app
+from mseg._internal.scripts import (
+    permutation_anova_app,
+    rbeast_app,
+    ruptures_app,
+)
 
 
 def main() -> None:
     pg = st.navigation(
-        [st.Page(rbeast_app.__file__), st.Page(ruptures_app.__file__)]
+        [
+            st.Page(rbeast_app.__file__),
+            st.Page(ruptures_app.__file__),
+            st.Page(permutation_anova_app.__file__),
+        ]
     )
     pg.run()
 
